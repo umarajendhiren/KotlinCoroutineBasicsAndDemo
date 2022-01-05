7dddd898fb864be3fde16b6dd49cfd78459692ac
uma <uma2011ece@gmail.com>
2022-01-04 22:33:26 -0500

    Added CreatingFlow

--- app/src/main/java/com/androidapps/kotlincoroutine/CreatingFlow.kt
+++ app/src/main/java/com/androidapps/kotlincoroutine/CreatingFlow.kt
@@ -1,2 +1,25 @@
 package com.androidapps.kotlincoroutine
 
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        sendNumbers().collect {
+            println("Received $it")
+        }
+    }
+}
+
+fun sendNumbers()
+        = flowOf("One", "Two", "Three")
+
+//        = listOf(1, 2, 3).asFlow()
+
+//        = flow {
+//    for (i in 1..10)
+//        emit(i)
+//}
