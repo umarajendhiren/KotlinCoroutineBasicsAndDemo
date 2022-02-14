aab2fef5bec2f46e6c14ab2b32a504045c4d7bdd
uma <uma2011ece@gmail.com>
2022-02-14 10:46:58 -0500

    Added appCoroutinesDemo module

---
+++ githubapicoroutine/src/main/java/com/androidapps/githubapicoroutine/contributors/main.kt
@@ -0,0 +1,13 @@
+package contributors
+
+
+
+
+fun main() {
+    setDefaultFontSize(18f)
+    ContributorsUI().apply {
+        pack()
+        setLocationRelativeTo(null)
+        isVisible = true
+    }
+}
