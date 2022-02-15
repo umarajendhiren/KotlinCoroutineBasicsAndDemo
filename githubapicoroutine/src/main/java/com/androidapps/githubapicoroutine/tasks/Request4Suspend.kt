f7dca3d592a4f30d4e9e26a7b6eff7abf977aac1
uma <uma2011ece@gmail.com>
2022-02-15 15:00:45 -0500

    Updated without Demo

--- githubapicoroutine/src/main/java/com/androidapps/githubapicoroutine/tasks/Request4Suspend.kt
+++
@@ -1,7 +0,0 @@
-package tasks
-
-import contributors.*
-
-suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
-}
--- githubapicoroutine/src/main/java/com/androidapps/githubapicoroutine/tasks/Request5NotCancellable.kt
+++
@@ -1,9 +0,0 @@
-package tasks
-
-import contributors.*
-import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
-
-suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
-}
